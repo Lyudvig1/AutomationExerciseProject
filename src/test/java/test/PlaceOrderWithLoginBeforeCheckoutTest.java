5d38adaf69a7b260f055f44fa3894c8e436d3482
Lyudvig1 <103680652+Lyudvig1@users.noreply.github.com>
2023-08-11 14:14:53 +0400

    Update PlaceOrderWithLoginBeforeCheckoutTest.java

--- src/test/java/test/PlaceOrderWithLoginBeforeCheckoutTest.java
+++ src/test/java/test/PlaceOrderWithLoginBeforeCheckoutTest.java
@@ -3,26 +3,27 @@ package test;
 import org.testng.annotations.Test;
 import testdata.DynamicData;
 import testdata.StaticData;
+import static share.App.*;
 
 
 public class PlaceOrderWithLoginBeforeCheckoutTest extends BaseTest {
     @Test(description = "TC_16: Place Order Login before Checkout")
     public void orderAfterLogin() {
-        app.homePage.navigateToSignupLoginPage();
-        app.loginPage.login(StaticData.DEFAULT_USER_EMAIL, StaticData.DEFAULT_USER_PASSWORD);
-        app.homePage.verifyTopUserNameVisibility()
+        homePage.navigateToSignupLoginPage();
+        loginPage.login(StaticData.DEFAULT_USER_EMAIL, StaticData.DEFAULT_USER_PASSWORD);
+        homePage.verifyTopUserNameVisibility()
                 .navigateToProductsPage();
-        app.productsPage.addProductToCart();
-        app.homePage.navigateToCartPage();
-        app.cartPage.verifyCartPageVisibility()
+        productsPage.addProductToCart();
+        homePage.navigateToCartPage();
+        cartPage.verifyCartPageVisibility()
                 .verifyAddedProductsVisibilityInCart()
                 .navigateToCheckoutPage();
-        app.checkoutPage.verifyAddressDetails()
+        checkoutPage.verifyAddressDetails()
                 .verifyAddedProductsVisibilityInCheckoutPage()
                 .addCommentToTextAreaAndGoToPaymentPage(DynamicData.companyName);
-        app.paymentPage.fillPaymentDetails(DynamicData.userName, DynamicData.randomNumber)
+        paymentPage.fillPaymentDetails(DynamicData.userName, DynamicData.randomNumber)
                 .verifySuccessMessageVisibility(StaticData.SUCCESS_MESSAGE);
-        app.homePage.deleteAccount();
-        app.deleteAccountPage.verifyDeletedAccountHeaderIsVisibleAndContinue();
+        homePage.deleteAccount();
+        deleteAccountPage.verifyDeletedAccountHeaderIsVisibleAndContinue();
     }
 }
