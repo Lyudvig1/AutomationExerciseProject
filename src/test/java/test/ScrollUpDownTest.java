404e84371b1f0a6eda8802c70e69442d4c214cfb
Lyudvig1 <103680652+Lyudvig1@users.noreply.github.com>
2023-08-11 14:15:44 +0400

    Update ScrollUpDownTest.java

--- src/test/java/test/ScrollUpDownTest.java
+++ src/test/java/test/ScrollUpDownTest.java
@@ -1,11 +1,12 @@
 package test;
 
 import org.testng.annotations.Test;
+import static share.App.*;
 
 public class ScrollUpDownTest extends BaseTest {
     @Test(description = "TC_25: Verify Scroll Up using 'Arrow' button and Scroll Down functionality")
    public void verifyScrollUpDownWithArrowButton() {
-        app.homePage.scrollDownAndVerifyFooterHeader()
+        homePage.scrollDownAndVerifyFooterHeader()
                 .clickArrowToMoveUpwardAndVerifySliderText();
     }
 }
