dfafccea416f60001bfd8afd71913c2750025c32
Lyudvig1 <103680652+Lyudvig1@users.noreply.github.com>
2023-08-11 14:16:43 +0400

    Update SearchProductTest.java

--- src/test/java/test/SearchProductTest.java
+++ src/test/java/test/SearchProductTest.java
@@ -2,12 +2,13 @@ package test;
 
 import org.testng.annotations.Test;
 import testdata.StaticData;
+import static share.App.*;
 
 public class SearchProductTest extends BaseTest {
     @Test(description = "TC_9: Search Product")
    public void searchProduct() {
-        app.homePage.navigateToProductsPage();
-        app.productsPage.verifyAllProductsHeaderVisibility()
+        homePage.navigateToProductsPage();
+        productsPage.verifyAllProductsHeaderVisibility()
                 .searchProduct(StaticData.PRODUCT_NAME)
                 .verifySearchedProductsHeaderVisibility()
                 .verifyAllProductsAreVisibleInSearchResults();
