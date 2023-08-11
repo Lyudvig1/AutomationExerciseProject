0f0f0a2fcb9997c6d013dc9772bd121998997f6d
Lyudvig1 <103680652+Lyudvig1@users.noreply.github.com>
2023-08-11 13:59:45 +0400

    Update PlaceOrderWithLoginBeforeCheckoutTest.java

--- src/test/java/test/PlaceOrderWithLoginBeforeCheckoutTest.java
+++ src/test/java/test/PlaceOrderWithLoginBeforeCheckoutTest.java
@@ -10,18 +10,18 @@ public class PlaceOrderWithLoginBeforeCheckoutTest extends BaseTest {
     public void orderAfterLogin() {
         app.homePage.navigateToSignupLoginPage();
         app.loginPage.login(StaticData.DEFAULT_USER_EMAIL, StaticData.DEFAULT_USER_PASSWORD);
-        app.homePage.verifyTopUserNameVisibility();
-        app.homePage.navigateToProductsPage();
+        app.homePage.verifyTopUserNameVisibility()
+                .navigateToProductsPage();
         app.productsPage.addProductToCart();
         app.homePage.navigateToCartPage();
-        app.cartPage.verifyCartPageVisibility();
-        app.cartPage.verifyAddedProductsVisibilityInCart();
-        app.cartPage.navigateToCheckoutPage();
-        app.checkoutPage.verifyAddressDetails();
-        app.checkoutPage.verifyAddedProductsVisibilityInCheckoutPage();
-        app.checkoutPage.addCommentToTextAreaAndGoToPaymentPage(DynamicData.companyName);
-        app.paymentPage.fillPaymentDetails(DynamicData.userName, DynamicData.randomNumber);
-        app.paymentPage.verifySuccessMessageVisibility(StaticData.SUCCESS_MESSAGE);
+        app.cartPage.verifyCartPageVisibility()
+                .verifyAddedProductsVisibilityInCart()
+                .navigateToCheckoutPage();
+        app.checkoutPage.verifyAddressDetails()
+                .verifyAddedProductsVisibilityInCheckoutPage()
+                .addCommentToTextAreaAndGoToPaymentPage(DynamicData.companyName);
+        app.paymentPage.fillPaymentDetails(DynamicData.userName, DynamicData.randomNumber)
+                .verifySuccessMessageVisibility(StaticData.SUCCESS_MESSAGE);
         app.homePage.deleteAccount();
         app.deleteAccountPage.verifyDeletedAccountHeaderIsVisibleAndContinue();
     }
