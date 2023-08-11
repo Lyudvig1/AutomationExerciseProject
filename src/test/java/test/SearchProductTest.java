336c017d0abfd079f03ec8166f7e52110e05b864
Lyudvig1 <103680652+Lyudvig1@users.noreply.github.com>
2023-08-11 14:07:11 +0400

    Update SearchProductTest.java

--- src/test/java/test/SearchProductTest.java
+++ src/test/java/test/SearchProductTest.java
@@ -5,11 +5,11 @@ import testdata.StaticData;
 
 public class SearchProductTest extends BaseTest {
     @Test(description = "TC_9: Search Product")
-    public void searchProduct() {
+   public void searchProduct() {
         app.homePage.navigateToProductsPage();
-        app.productsPage.verifyAllProductsHeaderVisibility();
-        app.productsPage.searchProduct(StaticData.PRODUCT_NAME);
-        app.productsPage.verifySearchedProductsHeaderVisibility();
-        app.productsPage.verifyAllProductsAreVisibleInSearchResults();
+        app.productsPage.verifyAllProductsHeaderVisibility()
+                .searchProduct(StaticData.PRODUCT_NAME)
+                .verifySearchedProductsHeaderVisibility()
+                .verifyAllProductsAreVisibleInSearchResults();
     }
 }
